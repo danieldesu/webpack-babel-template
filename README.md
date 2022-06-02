c57509c7290521d4654e7a922f6bc331083d6398
Daniel Desu <danieldesuel@gmail.com>
2022-06-02 17:57:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple template to use Webpack & Babel in JS projects.
+
+Build with npm run build.
+Webpack server with npm run dev, running on port 3000.
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
